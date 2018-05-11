060805e8c4c52b2cc884786c3edeffbb829a4082
Joshua Magee <magee.joshua@yahoo.com>
2018-05-10 19:52:11 -0700

    [WIP] Add Al Bhed layer layout.

--- keyboards/planck/keymaps/jmagee/keymap.c
+++ keyboards/planck/keymaps/jmagee/keymap.c
@@ -21,6 +21,7 @@ extern keymap_config_t keymap_config;
 
 enum planck_layers {
   _QWERTY,
+  _ALBHED,
   _NUMPAD,
   _SYMBOLS,
   _LOWER,
@@ -31,6 +32,7 @@ enum planck_layers {
 
 enum planck_keycodes {
   QWERTY = SAFE_RANGE,
+  ALBHED,
   NUMPAD,
   SYMBOLS,
   PLOVER,
@@ -58,6 +60,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LEAD},
   {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______},
+  {NUMPAD , SYMBOLS, KC_LGUI, KC_LALT, KC_SPC , KC_TAB,  KC_BSPC, KC_ENT,  _______, _______, _______, _______}
+},
+
+/* Al Bhed
+ * ,-----------------------------------------------------------------------------------.
+ * |      |   X  |   F  |   A  |   N  |   D  |   O  |   I  |   E  |   U  |   B  |Leader|
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |EsCtl |   Y  |   C  |   T  |   V  |   K  |   R  |   Z  |   G  |   M  |   ;  |  "   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   W  |   Q  |   L  |   J  |   P  |   H  |   S  |   ,  |   .  |   /  |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |NumPad| Sym  | GUI  | Alt  |Space | Tab  |Bksp  |Enter | Comp |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_ALBHED] = {
+  {_______, KC_X,    KC_F,    KC_A,    KC_N,    KC_D,    KC_O,    KC_I,    KC_E,    KC_U,    KC_B,    KC_LEAD},
+  {KC_ESC,  KC_Y,    KC_C,    KC_T,    KC_V,    KC_K,    KC_R,    KC_Z,    KC_G,    KC_M,    KC_SCLN, KC_QUOT},
+  {KC_LSFT, KC_W,    KC_Q,    KC_L,    KC_J,    KC_P,    KC_H,    KC_S,    KC_COMM, KC_DOT,  KC_SLSH, _______},
   {NUMPAD , SYMBOLS, KC_LGUI, KC_LALT, KC_SPC , KC_TAB,  KC_BSPC, KC_ENT,  _______, _______, _______, _______}
 },
 
