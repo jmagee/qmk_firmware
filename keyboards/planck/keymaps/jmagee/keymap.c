fbe4e415e4815de003ffa8a26c6a061e3c92f0a9
Joshua Magee <magee.joshua@yahoo.com>
2018-05-10 21:52:49 -0700

    Define and use ESCTRL - Escape on tap, control on hold.

--- keyboards/planck/keymaps/jmagee/keymap.c
+++ keyboards/planck/keymaps/jmagee/keymap.c
@@ -46,6 +46,9 @@ enum planck_keycodes {
 #define T_SYM    ST(_SYMBOLS, SYMBOLS)
 #define T_NAVI   ST(_NAVI, NAVI)
 
+/* Escape on tap, control on hold.*/
+#define ESCTRL   CTL_T(KC_ESC)
+
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 
@@ -64,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = {
   {_______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LEAD},
-  {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {ESCTRL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______},
   {T_NUMPAD,T_SYM  , KC_LGUI, KC_LALT, KC_SPC , KC_TAB,  KC_BSPC, KC_ENT,  T_NAVI , _______, _______, _______}
 },
@@ -82,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ALBHED] = {
   {QWERTY , KC_X,    KC_F,    KC_A,    KC_N,    KC_D,    KC_O,    KC_I,    KC_E,    KC_U,    KC_B,    KC_LEAD},
-  {KC_ESC,  KC_Y,    KC_C,    KC_T,    KC_V,    KC_K,    KC_R,    KC_Z,    KC_G,    KC_M,    KC_SCLN, KC_QUOT},
+  {ESCTRL,  KC_Y,    KC_C,    KC_T,    KC_V,    KC_K,    KC_R,    KC_Z,    KC_G,    KC_M,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_W,    KC_Q,    KC_L,    KC_J,    KC_P,    KC_H,    KC_S,    KC_COMM, KC_DOT,  KC_SLSH, _______},
   {T_NUMPAD,T_SYM  , KC_LGUI, KC_LALT, KC_SPC , KC_TAB,  KC_BSPC, KC_ENT,  _______, _______, _______, _______}
 },
