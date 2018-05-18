5c47cbbfaacd4f7493ae0d2905684a10d6f22bb6
Joshua Magee <magee.joshua@yahoo.com>
2018-05-17 23:14:22 -0800

    Remove unused code.

--- keyboards/planck/keymaps/jmagee/keymap.c
+++ keyboards/planck/keymaps/jmagee/keymap.c
@@ -133,13 +133,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 },
 };
 
-uint32_t layer_state_set_user(uint32_t state) {
-  return state;
-#if 0
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-#endif
-}
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (is_layer_keycode(keycode)) {
     if (record->event.pressed) {
