e58bc34326374901d4d569163a76ba230f97bb8b
Joshua Magee <magee.joshua@yahoo.com>
2018-07-16 21:09:41 -0700

    Add KC_LOCK to planck keymap.

--- keyboards/planck/keymaps/jmagee/keymap.c
+++ keyboards/planck/keymaps/jmagee/keymap.c
@@ -38,8 +38,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT_planck_grid(
   BASE   , KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LEAD,
   ESCTRL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
-  T_NUMPAD,T_SYM  , KC_LGUI, KC_LALT, KC_SPC , KC_TAB,  KC_BSPC, KC_ENT,  KC_RALT, T_NAVI , T_MOUSE, T_BHED 
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LOCK,
+  T_NUMPAD,T_SYM  , KC_LGUI, KC_LALT, KC_SPC , KC_TAB,  KC_BSPC, KC_ENT,  KC_RALT, T_NAVI , T_MOUSE, T_BHED
 ),
 
 /* Al Bhed
--- keyboards/planck/keymaps/jmagee/rules.mk
+++ keyboards/planck/keymaps/jmagee/rules.mk
@@ -1,4 +1,5 @@
 MOUSEKEY_ENABLE = yes
 NKRO_ENABLE = yes
+KEY_LOCK_ENABLE = yes
 
 EXTRAFLAGS += -flto
