04d256781817bc806cfe99a113636b6248d10635
Joshua Magee <magee.joshua@yahoo.com>
2018-08-01 21:12:15 -0700

    Only disable AUDIO and RGBLIGHGT for vitamins_included.

--- layouts/community/ortho_4x12/jmagee/rules.mk
+++ layouts/community/ortho_4x12/jmagee/rules.mk
@@ -1,9 +1,10 @@
+CONSOLE_ENABLE = yes
+DEBUG_ENABLE = yes
 MOUSEKEY_ENABLE = yes
 NKRO_ENABLE = yes
 #KEY_LOCK_ENABLE = yes
 
-# Temporary
-AUDIO_ENABLE = no
-RGBLIGHT_ENABLE = no
-CONSOLE_ENABLE = yes
-DEBUG_ENABLE = yes
+ifeq ($(strip $(KEYBOARD)), vitamins_included/rev1)
+	AUDIO_ENABLE = no
+	RGBLIGHT_ENABLE = no
+endif
