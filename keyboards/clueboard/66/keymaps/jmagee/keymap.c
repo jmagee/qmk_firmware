e02b7aead83d1ea7da13670bb2d2f7a00c1248b7
Joshua Magee <magee.joshua@yahoo.com>
2019-01-11 17:54:09 -0800

    Tweak clueboard layouts; Add RGB settings for random per-keypress lighting.

--- keyboards/clueboard/66/keymaps/jmagee/keymap.c
+++ keyboards/clueboard/66/keymaps/jmagee/keymap.c
@@ -4,6 +4,10 @@
 #include "layers.h"
 #include "leader.h"
 
+static uint32_t mode;
+static uint16_t h;
+static uint8_t s, v;
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwertz. */
 [_BASE] = LAYOUT(
@@ -11,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB, DE_Q,   DE_W,   DE_E,   DE_R,   DE_T,   DE_Z,   DE_U,   DE_I,   DE_O,   DE_P,   DE_UE,  DE_PLUS,XXXXXXX,                KC_PGDN,
   ESCTRL, DE_A,   DE_S,   DE_D,   DE_F,   DE_G,   DE_H,   DE_J,   DE_K,   DE_L,   DE_OE,  DE_AE  ,DE_HASH,KC_ENT,
   KC_LSFT,DE_LESS,DE_Y,   DE_X,   DE_C,   DE_V,   DE_B,   DE_N,   DE_M,   DE_COMM,DE_DOT, DE_MINS,KC_LEAD,KC_RSFT,        KC_UP,
-  KC_LOCK,KC_LGUI,KC_LALT,KC_HYPR,        KC_SPC, KC_ENT,                         KC_ALGR,KC_MEH, T_FUNC ,T_BHED ,KC_LEFT,KC_DOWN,KC_RGHT),
+  KC_LOCK,KC_LGUI,KC_LALT,KC_HYPR,        KC_SPC, KC_ENT,                         KC_ALGR,KC_MEH, FUNC   ,ALBHED ,KC_LEFT,KC_DOWN,KC_RGHT),
 
 /* Al Bhed. */
 [_ALBHED] = LAYOUT(
@@ -19,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,DE_W,   DE_F,   DE_A,   DE_N,   DE_D,   DE_O,   DE_I,   DE_E,   DE_U,   DE_B,   _______,_______,XXXXXXX,                _______,
   _______,DE_Z,   DE_C,   DE_T,   DE_V,   DE_K,   DE_R,   DE_Y,   DE_G,   DE_M,   _______,_______,_______,_______,
   _______,_______,DE_W,   DE_Q,   DE_L,   DE_J,   DE_P,   DE_H,   DE_S,   _______,_______,_______,_______,_______,        _______,
-  _______,_______,_______,_______,        _______,_______,                        _______,_______,_______,_______,_______,_______,_______),
+  _______,_______,_______,_______,        _______,_______,                        _______,_______,_______,BASE   ,_______,_______,_______),
 
 /* Keymap _FL: Function Layer */
 [_FUNC] = LAYOUT(
@@ -27,12 +31,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,XXXXXXX,                KC_VOLD,
   _______,_______,_______,_______,_______,_______,_______,KC_MPRV,KC_MSTP,KC_MPLY,KC_MNXT,_______,_______,_______,
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        KC_PGUP,
-  _______,_______,_______,_______,        _______,_______,                        _______,_______,_______,_______,KC_HOME,KC_PGDN,KC_END),
+  _______,_______,_______,_______,        _______,_______,                        _______,_______,BASE   ,_______,KC_HOME,KC_PGDN,KC_END),
 
 /* GOD: Control layer */
 [_GOD] = LAYOUT(
   BL_STEP,RGB_M_P,RGB_M_B,RGB_M_R,RGB_M_SW,RGB_M_SN,RGB_M_K,RGB_M_X,RGB_M_G,_______,_______,_______,_______,_______,RGB_TOG,        RGB_VAI,
-  _______,_______,_______,_______,RESET,  _______,_______,_______,_______,_______,_______,_______,_______,_______,                RGB_VAD,
+  BASE   ,_______,_______,_______,RESET,  _______,_______,_______,_______,_______,_______,_______,_______,_______,                RGB_VAD,
   _______,RGB_M_R,RGB_M_SN,_______,_______,_______,_______,_______,RGB_M_K,_______,_______,_______,_______,_______,
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        RGB_SAI,
   _______,_______,_______,_______,        RGB_MOD,   RGB_MOD,                     _______,_______,_______,_______,RGB_HUD,RGB_SAD,RGB_HUI),
@@ -49,13 +53,15 @@ void matrix_init_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (is_layer_keycode(keycode)) {
-    if (record->event.pressed) {
-      activate_layer(keycode_to_layer(keycode));
-      return false;
-    }
+  if (record->event.pressed) {
+    rgblight_mode(RGBLIGHT_MODE_STATIC_LIGHT);
+    rgblight_sethsv(rand() % 360, rand() % 255, rand() % 255);
+  } else {
+    rgblight_mode(mode);
+    rgblight_sethsv(h, s, v);
   }
-  return true;
+
+  return process_custom_keycodes(keycode, record);
 }
 
 void matrix_scan_user(void) {
@@ -73,22 +79,31 @@ void matrix_scan_user(void) {
 uint32_t layer_state_set_user(uint32_t state) {
   switch (biton32(state)) {
     case _BASE:
-      /*rgblight_mode(4);*/
-      rgblight_mode(RGBLIGHT_MODE_RAINBOW_SWIRL);
+      mode = RGBLIGHT_MODE_RAINBOW_SWIRL;
       break;
     case _ALBHED:
-      rgblight_mode(1);
-      rgblight_sethsv(42, 242, 255);
+      mode = 1;
+      h = 42;
+      s = 242;
+      v = 255;
       break;
     case _FUNC:
-      rgblight_mode(1);
-      rgblight_sethsv(209, 244, 255);
+      mode = 1;
+      h = 209;
+      s = 244;
+      v = 255;
       break;
     case _GOD:
-      rgblight_mode(1);
-      rgblight_sethsv(189, 71, 100);
+      mode = 1;
+      h = 189;
+      s = 71;
+      v = 100;
       break;
   }
+
+  rgblight_mode(mode);
+  rgblight_sethsv(h, s, v);
+
   return state;
 }
 /* vim: set nowrap : */
--- keyboards/clueboard/66/keymaps/jmagee/rules.mk
+++ keyboards/clueboard/66/keymaps/jmagee/rules.mk
@@ -1,3 +1,4 @@
 NKRO_ENABLE = yes
 KEY_LOCK_ENABLE = yes
 COMMAND_ENABLE = no
+CONSOLE_ENABLE = no # Console for debug(+400)
